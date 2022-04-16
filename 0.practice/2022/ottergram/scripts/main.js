31f58c32eb9b5c0618b695ce732905fe0348b4ff
stothey0804 <stothey0804@gmail.com>
2022-04-16 15:19:14 +0900

    js action 추가

--- 0.practice/2022/ottergram/scripts/main.js
+++ 0.practice/2022/ottergram/scripts/main.js
@@ -1,15 +1,32 @@
 // use strict - 엄격모드 사용. 
-
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const ESC_KEY = 27;
+
+
+const getThumbnailArray = () => {
+    'use strict';
+    // const thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    // const thumbnail_array = [].slice.call(thumbnails);
+    return [...document.querySelectorAll(THUMBNAIL_LINK_SELECTOR)];
+}
+// class 추가
+const hideDetails = () => {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+const showDetails = () => {
+    'use strict';
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
 
 // 실행 이벤트 설정!
-function initializeEvents() {
+const initializeEvents = () => {
     'use strict';
-    const thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-    thumbnails.forEach( element => addThumbClickHandler(element));
-
+    getThumbnailArray().forEach(addThumbClickHandler);
+    addKeyPressHandler();
 }
 
 const imageFromThumb = (thumbnail) => {
@@ -32,6 +49,19 @@ const addThumbClickHandler = (thumb) => {
     thumb.addEventListener('click', (event) => {
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
+    });
+}
+
+const addKeyPressHandler = () => {
+    'use strict';
+    // key up: 키를 땠을 때. key down: 눌렀을 때, key press: 문자키일 경우 같이 전송
+    document.body.addEventListener('keyup', (event) => {
+        event.preventDefault();
+        if(event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+        // console.log(event.keyCode);
     });
 }
 
