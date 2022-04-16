b3f6b8d14982d4bdfb82259a33853f04448d4a1f
stothey0804 <stothey0804@gmail.com>
2022-04-16 14:37:47 +0900

    html 출력 함수 추가

--- 0.practice/refactoring/chapter1/exam1/statement.js
+++ 0.practice/refactoring/chapter1/exam1/statement.js
@@ -1,9 +1,22 @@
 import createStatementData from './createStatementData.js';
 
 function statement(invoice, plays) {
-    return renderPlainText(createStatement(invoice, plays));
+    return renderHtml(createStatement(invoice, plays));
 }
 // render
+function renderHtml(data) {
+    let result = `<h1>청구내역 (고객명: ${data.customer}</h1>\n`;
+    result += `<table>\n`;
+    result += `<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>\n`;
+    for (let perf of data.performances) {
+        result += `<tr><td>${perf.play.name}</td><td>${usd(perf.amount)}</td><td>${perf.audience}석</td></tr>\n`
+    }
+    result += `</table>\n`;
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>\n`;
+    return result;
+}
+
 function renderPlainText(data) { 
     let result = `청구내역 (고객명: ${data.customer})\n`;
 
@@ -14,9 +27,9 @@ function renderPlainText(data) {
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}점`;
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat("es-US",
-            { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
-    }
+function usd(aNumber) {
+    return new Intl.NumberFormat("es-US",
+        { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
 }
