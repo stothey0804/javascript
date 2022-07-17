745b7261682fc052a063635e3c465f02571bc0fd
Seyoung Kim <stothey0804@gmail.com>
2022-07-17 17:46:32 +0900

    4. create 액션 생성

--- 0.practice/redux_crud/index.js
+++ 0.practice/redux_crud/index.js
@@ -9,7 +9,8 @@ const init = () => {
     article();
     //subscribe
     store.subscribe(article);
-};
+    store.subscribe(TOC);
+}
 
 const subject = () => {
     const state = store.getState();
@@ -22,13 +23,31 @@ const subject = () => {
 }
 
 
-document.getElementById('toc').addEventListener('click', function (e) {
+document.getElementById('toc').addEventListener('click', (e) => {
     e.preventDefault;
     const targetId = e.target.dataset.id;
     const state = store.getState();
     if (typeof targetId != 'undefined') {
-        store.dispatch({type:'CHANGE_PAGE', id: state.contents[targetId].id});
+        store.dispatch({type:'SELECT', id: state.contents[targetId].id});
     }
+});
+
+document.getElementById('control').addEventListener('click', (event) => {
+    event.preventDefault;
+    const targetAction = e.target.dataset.action;
+    if(typeof targetAction == 'create') {
+        //
+        
+    }
+});
+
+// submit
+document.addEventListener('submit', (event) => {
+    event.preventDefault;
+    const _title = e.target.title.value;
+    const _desc = e.target.desc.value;
+    console.log(_title, _desc);
+    store.dispatch({type:'CREATE', title: _title, desc: _desc});
 });
 
 
@@ -57,20 +76,43 @@ const TOC = () => {
 const control = () => {
     document.querySelector('#control').innerHTML = `
     <ul>
-        <li><a href="/create">create</a></li>
-        <li><input type="button" value="delete"></li>
+        <li data-action="create"><a href="/create">create</a></li>
+        <li data-action="delete"><input type="button" value="delete"></li>
     </ul>
     `;
 }
 const article = () => {
     const state = store.getState();
     const currId = state.currentPageId-1;
-    document.querySelector('#content').innerHTML = `
-    <article>
-        <h2>${state.contents[currId].title}</h2>
-        ${state.contents[currId].desc}
-    </article>
-    `
+    let content;
+
+    if(state.mode === 'create') {
+        content = `
+        <article>
+            <form id="createForm">
+                <p>
+                    <input type="text" name="title" placeholder="title">
+                </p>
+                <p>
+                   <textarea name="desc" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+        </article>
+        `;
+
+    } else if(state.mode === 'read') {
+        content = `
+        <article>
+            <h2>${state.contents[currId].title}</h2>
+            ${state.contents[currId].desc}
+        </article>
+        `
+    }
+
+    document.querySelector('#content').innerHTML = content;
 }
 
 init();
--- 0.practice/redux_crud/reducer.js
+++ 0.practice/redux_crud/reducer.js
@@ -2,18 +2,32 @@ const reducer = (state, action) => {
     // state 초기값
     if (state === undefined) {
         return {
+            currentPageId: 1, // selected id 
+            mode: 'create',
+            maxId: 2,
+            // 초기 제목
             subject: { title: 'WEB', desc: 'Hello, WEB!' },
             contents: [
                 {id:1, title:'HTML', desc:'HTML is ..'},
                 {id:2, title:'CSS', desc:'CSS is ..'},
             ],
-            currentPageId: 1,
         }
     }
 
     let newState;
-    if(action.type === 'CHANGE_PAGE') {
-        newState = {...state, currentPageId: action.id || 1}
+    if(action.type === 'SELECT') {
+        newState = {...state, mode: 'read', currentPageId: action.id || 1}
+    } else if (action.type === 'CREATE') {
+        // id length update
+        const newMaxId = state.maxId + 1;
+        // content update 
+        const newContent = {
+            id: newMaxId,
+            title: action.title,
+            desc: action.desc,
+        }
+        newState = { ...state, maxId: newMaxId, mode: 'read' }
+        newState.contents.push(newContent);
     }
 
     return newState;
