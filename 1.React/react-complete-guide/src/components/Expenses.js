556b47f4f0bd2716d3060b383a46e8d32284c460
stothey0804 <stothey0804@gmail.com>
2022-11-20 17:49:55 +0900

    리액트 가이드 강의 예제 앱 생성

---
+++ 1.React/react-complete-guide/src/components/Expenses.js
@@ -0,0 +1,17 @@
+import './Expenses.css'
+import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
+
+const Expenses = (props) => {
+  return (
+    <Card className={`expenses`}>
+      <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount}></ExpenseItem>
+      <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount}></ExpenseItem>
+      <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount}></ExpenseItem>
+      <ExpenseItem date={props.items[3].date} title={props.items[3].title} amount={props.items[3].amount}></ExpenseItem>
+      <ExpenseItem date={props.items[4].date} title={props.items[4].title} amount={props.items[4].amount}></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
