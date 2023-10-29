29fa6ef6f5c204e832f273432bc136474ccdd0c8
stothey0804 <stothey0804@gmail.com>
2023-10-29 18:48:00 +0900

    menu order app > init components

---
+++ 1.React/menu-order-app/src/components/UI/Modal.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from './Modal.module.css'
+
+const Modal = () => {
+  return (<>
+    <div className={classes.modal}>
+
+    </div>
+  </>);
+};
+
+export default Modal;
